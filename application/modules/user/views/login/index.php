3abc24d4fb3fb9e6dcb2156dafcf46008206456b
JayDee <fettsack@gc-community.org>
2015-09-26 01:09:53 +0200

    update userModul
    add google+

--- application/modules/user/views/login/index.php
+++ application/modules/user/views/login/index.php
@@ -1,3 +1,57 @@
+<!-- Platzieren Sie dieses asynchrone JavaScript unmittelbar vor Ihrem </body>-Tag -->
+<script type="text/javascript">
+    (function() {
+        var po = document.createElement('script'); 
+        po.type = 'text/javascript'; 
+        po.async = true;
+        po.src = 'https://apis.google.com/js/client:plusone.js';
+        var s = document.getElementsByTagName('script')[0]; 
+        s.parentNode.insertBefore(po, s);
+    })();
+     
+    function signinCallback(authResult) {
+        if (authResult['access_token']) {
+        // Autorisierung erfolgreich
+        // Nach der Autorisierung des Nutzers nun die Anmeldeschaltfläche ausblenden, zum Beispiel:
+            document.getElementById('signinButton').setAttribute('style', 'display: none');
+        } else if (authResult['error']) {
+        // Es gab einen Fehler.
+        // Mögliche Fehlercodes:
+        //   "access_denied" – Der Nutzer hat den Zugriff für Ihre App abgelehnt.
+        //   "immediate_failed" – Automatische Anmeldung des Nutzers ist fehlgeschlagen.
+        // console.log('Es gab einen Fehler: ' + authResult['Fehler']);
+        }
+    }
+</script>
+    
+<script type="text/javascript">
+    function disconnectUser(access_token) {
+        var revokeUrl = 'https://accounts.google.com/o/oauth2/revoke?token=' +
+        access_token;
+
+      // Führen Sie einen asynchrone GET-Anfrage durch.
+        $.ajax({
+            type: 'GET',
+            url: revokeUrl,
+            async: false,
+            contentType: "application/json",
+            dataType: 'jsonp',
+            success: function(nullResponse) {
+            // Führen Sie jetzt nach der Trennung des Nutzers eine Aktion durch.
+            // Die Reaktion ist immer undefiniert.
+            },
+            error: function(e) {
+            // Handhaben Sie den Fehler.
+            // console.log(e);
+            // Wenn es nicht geklappt hat. könnten Sie Nutzer darauf hinweisen, wie die manuelle Trennung erfolgt.
+            // https://plus.google.com/apps
+            }
+        });
+    }
+// Sie könnten die Trennung über den Klick auf eine Schaltfläche auslösen.
+    $('#revokeButton').click(disconnectUser);
+</script>
+
 <?php if($this->getUser() == null): ?>
 <div class="panel panel-info">
     <div class="panel-heading">
@@ -6,6 +60,18 @@
     <div class="panel-body">
         <div class="row">
             <div class="col-md-4" >
+                
+                <span id="signinButton">
+                    <span
+                      class="g-signin"
+                      data-callback="signinCallback"
+                      data-clientid="532260975165-7cb2to2sdrnav7i3ul8gch18o24mq9vn.apps.googleusercontent.com"
+                      data-cookiepolicy="single_host_origin"
+                      data-requestvisibleactions="http://schemas.google.com/AddActivity"
+                      data-scope="https://www.googleapis.com/auth/plus.login">
+                    </span>
+                </span>
+                
                 <a href="#"><img src="<?=$this->getModuleUrl('static/images/facebook/fb.png') ?>"/></a><br/>
                 <a href="#"><img src="<?=$this->getModuleUrl('static/images/google/gplus.png') ?>" /></a><br/>
                 <a href="#"><img src="<?=$this->getModuleUrl('static/images/twitter/tw.png') ?>" /></a>
