1ac23dfe59f5d9215cd0854d299eefc02f2e2ef1
JayDee <fettsack@gc-community.org>
2015-11-19 22:55:15 +0100

    create Design
    design wieder aufgegriffen ...

---
+++ application/layouts/simpleBW/config/config.php
@@ -0,0 +1,9 @@
+<?php
+$config['name'] = 'Ilch-Clan';
+$config['author'] = 'Ilch.de';
+$config['link'] = 'http://ilch.de';
+$config['desc'] = 'simpleBW';
+//$config['layouts']['blog'] = array(array('module' => 'user', 'controller' => 'panel')); //only for example
+$config['layouts']['home'] = array(array('module' => 'forum')); //only for example
+//$config['layouts']['index_full'] = array(array('module' => 'guestbook', 'controller' => 'index', 'action' => 'newentry'));
+//$config['modulekey'] = 'Name of Module';
